8952734207364ee87e478e4df94084341aa8f426
Akshaytyag <you@example.com>
2020-11-17 21:47:17 +0530

    Updates

---
+++ precache-manifest.834e2abdf700253a91c679da79240ecf.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecccb5bacc51e7907e1688a59dfbe71a",
+    "url": "/MapQuiz/index.html"
+  },
+  {
+    "revision": "4cd3735fa908422eea34",
+    "url": "/MapQuiz/static/css/2.e9085934.chunk.css"
+  },
+  {
+    "revision": "896de1223f49accf22b4",
+    "url": "/MapQuiz/static/css/main.ab733aa4.chunk.css"
+  },
+  {
+    "revision": "4cd3735fa908422eea34",
+    "url": "/MapQuiz/static/js/2.08645427.chunk.js"
+  },
+  {
+    "revision": "b2180d96f1a20ded8f1b593a2a9cbecb",
+    "url": "/MapQuiz/static/js/2.08645427.chunk.js.LICENSE"
+  },
+  {
+    "revision": "896de1223f49accf22b4",
+    "url": "/MapQuiz/static/js/main.23811c51.chunk.js"
+  },
+  {
+    "revision": "7988e632c433c85bc849",
+    "url": "/MapQuiz/static/js/runtime-main.b9d23e80.js"
+  },
+  {
+    "revision": "f05e37d5dde17938e5efbd6ff5876374",
+    "url": "/MapQuiz/static/media/answer.f05e37d5.png"
+  },
+  {
+    "revision": "c0302796e984ecbb92c4b065a1c1a92f",
+    "url": "/MapQuiz/static/media/background.c0302796.jpg"
+  },
+  {
+    "revision": "3219419957b7d03578da90e20d8a7401",
+    "url": "/MapQuiz/static/media/button-sound.32194199.mp3"
+  },
+  {
+    "revision": "ee6aadae18d9da389dc08dd8d72e3721",
+    "url": "/MapQuiz/static/media/correct-answer.ee6aadae.mp3"
+  },
+  {
+    "revision": "f06f3bfe9454949bce2baa2f2af4d2ec",
+    "url": "/MapQuiz/static/media/fiftyFifty.f06f3bfe.PNG"
+  },
+  {
+    "revision": "145bf2178ec018524dadd29e831cc8a3",
+    "url": "/MapQuiz/static/media/hints.145bf217.PNG"
+  },
+  {
+    "revision": "1618c77b6c5c11926819a8868a083031",
+    "url": "/MapQuiz/static/media/materialdesignicons-webfont.1618c77b.ttf"
+  },
+  {
+    "revision": "927457ed7478ec7e1580a2b88116f9c3",
+    "url": "/MapQuiz/static/media/materialdesignicons-webfont.927457ed.woff2"
+  },
+  {
+    "revision": "d6e3eba9b16453f51d4ef6e216bf89ff",
+    "url": "/MapQuiz/static/media/materialdesignicons-webfont.d6e3eba9.woff"
+  },
+  {
+    "revision": "f81583fc4d8aa5aa2820976561ea8aec",
+    "url": "/MapQuiz/static/media/materialdesignicons-webfont.f81583fc.eot"
+  },
+  {
+    "revision": "0ab14d143f1993fd0a065be0e0aa58a5",
+    "url": "/MapQuiz/static/media/options.0ab14d14.PNG"
+  },
+  {
+    "revision": "c7106542fc4e8075bf1bae4bf6854692",
+    "url": "/MapQuiz/static/media/wrong-answer.c7106542.mp3"
+  }
+]);
